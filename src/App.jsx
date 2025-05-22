e675fadef8a1363b5cff286098bcaaae5da9c7d3
Allen Chang <allenzx0202@gmail.com>
2025-05-23 04:48:58 +0800

    feat: 使用 Menterial UI Chart 完成圖表
    搭配 useSWR, axios, Exchange Rates Data API 完成。

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import CounterButton from "./components/CounterButton.jsx";
+import MarketCapChart from "./components/MarketCapChart.jsx";
 
 function App() {
   return (
     <>
       <CounterButton />
+      <MarketCapChart />
     </>
   );
 }
