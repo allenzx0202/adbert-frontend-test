80fe39e28519a3b8c2c7680685d75a409a894b84
Allen Chang <allenzx0202@gmail.com>
2025-05-23 05:54:07 +0800

    refactor: 改使用 coin gecko api 完成圖表實作
    因原本使用的 api 有限制流量怕影響圖表呈現，改用另外一種公開 api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import CounterButton from "./components/CounterButton.jsx";
-import MarketCapChart from "./components/MarketCapChart.jsx";
+import TradingVolumeChart from "./components/TradingVolumeChart.jsx";
 
 function App() {
   return (
     <>
       <CounterButton />
-      <MarketCapChart />
+      <TradingVolumeChart />
     </>
   );
 }
